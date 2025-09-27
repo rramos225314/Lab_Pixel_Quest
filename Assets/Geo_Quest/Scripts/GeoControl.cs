575f4959c731174c615893123ce478ec67c19114
rramos225314 <rramos225314@gmail.com>
2025-09-27 12:21:51 -0400

    9/27/2025

---
+++ Assets/Geo_Quest/Scripts/GeoControl.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeoControl : MonoBehaviour
+{
+    string World = "Hello";  // Start is called before the first frame update
+    void Start()
+    {
+        string Bread = "World";
+        Debug.Log(World+Bread);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
