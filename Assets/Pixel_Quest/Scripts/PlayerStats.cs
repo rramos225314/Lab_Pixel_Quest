b18fc652bd86c4714fa8a0f722db949efac03377
rramos225314 <rramos225314@gmail.com>
2025-11-08 15:27:56 -0500

    11/8/2025

--- Assets/Pixel_Quest/Scripts/PlayerStats.cs
+++ Assets/Pixel_Quest/Scripts/PlayerStats.cs
@@ -33,6 +33,10 @@ public class PlayerStats : MonoBehaviour
 
 
                 }
+                case "Health":
+                {
+                    break;
+                }
 
         }
     }
